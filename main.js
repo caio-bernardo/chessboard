58d4547fac33ad0388f31cdff09270a4933f16cf
Caio <caiovb@duck.com>
2024-02-03 19:28:47 -0300

    edit main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const board = document.createElement("table")
-board.setAttribute("id", "board")
+const board = document.createElement("table");
+board.setAttribute("id", "board");
 
-for (row=1; row <= 8; row++) {
-    const tablerow = document.createElement("tr")
-    for (cell=1;  cell<= 8; cell++) {
-        const square = document.createElement("td")
-        square.setAttribute("class", "square")
-        if (row % 2 == cell % 2) {
-            square.setAttribute("class", "dark-square")
-        } else {
-            square.setAttribute("class", "light-square")
-        }
-        tablerow.appendChild(square) 
+for (row = 1; row <= 8; row++) {
+  const tablerow = document.createElement("tr");
+  for (cell = 1; cell <= 8; cell++) {
+    const square = document.createElement("td");
+    square.setAttribute("class", "square");
+    if (row % 2 === cell % 2) {
+      square.setAttribute("class", "dark-square");
+    } else {
+      square.setAttribute("class", "light-square");
     }
-    board.appendChild(tablerow)
+    tablerow.appendChild(square);
+  }
+  board.appendChild(tablerow);
 }
 
-document.getElementById("root").appendChild(board)
+document.getElementById("root").appendChild(board);
